ed7b4cd6a102e221162c1b49b8de7311414d0d98
suraj <surajjosh7@gmail.com>
2023-10-02 00:26:40 +0530

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,59 @@
+
+
+let input = document.getElementById('inputfield');
+let submit = document.getElementById('sendmsg')
+let msgcontainer = document.getElementsByClassName('maincontainer')[0];
+let userss=document.getElementById('no')
+
+
+var socket = io()
+let userName;
+
+do {
+  userName = prompt("Please enter your name:");
+} while (!userName);
+socket.emit('name',userName)
+
+socket.on('name_br',function(value){
+    let joined_block=document.createElement('div');
+    joined_block.classList.add('msg3');
+    joined_block.innerHTML=value+" connected";
+    msgcontainer.appendChild(joined_block);
+})
+
+socket.on('no_of_users',function(value){
+    userss.innerHTML=`${value}`;
+})
+
+
+function sendMessage(){
+    let date = new Date();
+    let hour = date.getUTCHours();
+    let minutes=date.getUTCMinutes();
+    let newelement = document.createElement('div');
+    newelement.classList.add("msgs");
+    let suraj=input.value;
+    newelement.innerHTML=`<p class='name-of-user'>${userName}</p>${suraj}<p class='time'>${hour}:${minutes}</p>`;
+    input.value="";
+    socket.emit("user_name",`<p class='name-of-user'>${userName}</p>${suraj}<p class='time'>${hour}:${minutes}</p>`);
+    msgcontainer.appendChild(newelement)
+    
+
+}
+
+
+submit.addEventListener('click', sendMessage);
+
+input.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        sendMessage();
+    }
+});
+
+
+socket.on('broadcast',function(value){
+    let broadelement = document.createElement('div');
+    broadelement.classList.add("msgs2");
+    broadelement.innerHTML=value;
+    msgcontainer.appendChild(broadelement)
+})
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+let app = express()
+
+let user_name="";
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+let server = http.createServer(app);
+
+const io = require('socket.io')(server);
+
+let users=0;
+
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,'public')+'/index.html')
+})
+
+
+io.on('connection',function(socket){
+    console.log("user has been connected");
+    users++;
+
+    socket.on('name',function(value){
+        socket.broadcast.emit('name_br',value);
+    })
+
+    io.sockets.emit('no_of_users',users);
+
+
+    socket.on('user_name', function(value) {
+        user_name = value;
+        socket.broadcast.emit('broadcast', user_name);
+    });
+
+    
+
+    socket.on('disconnect',function(){
+        console.log("user is disconnected");
+        users--;
+    });
+});
+
+server.listen(4500,function(){
+    console.log("your server is running at http://localhost:4500")
+})
+
